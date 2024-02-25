b10638f71853997345c78d10270f1fe78a7cf497
mscha <m.saleh.sedeghi@gmail.com>
2024-02-26 00:41:32 +0330

    Fix problems of BankAccount.java and add getters

--- src/main/java/org/example/BankAccount.java
+++ src/main/java/org/example/BankAccount.java
@@ -1,32 +1,51 @@
 package org.example;
 
-import java.util.Optional;
-
 public class BankAccount {
     private String accountNumber;
     private double balance;
     private String accountHolderName;
-    public void deposit(int newBalance){
-        if (newBalance<0)
+
+
+    public void deposit (double amount){
+        if (amount < 0)
             throw new IllegalArgumentException("amount of deposit can not be negative");
         else
-            balance+= newBalance;
-    }
-    public void withdraw(double withdrawAmount){
-        if (withdrawAmount>balance)
-            throw new IllegalArgumentException("insufficient balance ");
-        else
-            balance-= withdrawAmount;
+            balance += amount;
     }
 
+    public void withdraw (double amount){
+        if (amount > balance)
+            throw new InsufficientFundsException("insufficient balance ");
+        else if (amount < 0)
+            throw new InsufficientFundsException("not valid amount ");
+        else
+            balance -= amount;
+    }
+
+    // Constructor
     public BankAccount(String accountNumber, String accountHolderName) {
         this.accountNumber = accountNumber;
         this.accountHolderName = accountHolderName;
+        balance = 0;
     }
 
     public BankAccount(String accountNumber, String accountHolderName, double balance) {
         this.accountNumber = accountNumber;
         this.accountHolderName = accountHolderName;
         this.balance = balance;
+    }
+
+
+    // Getters
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public String getAccountHolderName() {
+        return accountHolderName;
     }
 }
