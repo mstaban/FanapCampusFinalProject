496ba8f74728b9790481de666188032c8c94a98a
mstaban <mstabanfun@yahoo.com>
2024-02-26 03:27:46 -0800

    SavingAccount class code was written

--- src/main/java/org/example/bankAccounts/SavingAccount.java
+++ src/main/java/org/example/bankAccounts/SavingAccount.java
@@ -1,4 +1,37 @@
 package org.example.bankAccounts;
 
-public class SavingAccount {
+import org.example.exeptions.InsufficientFundsException;
+import org.example.exeptions.InvalidTransactionException;
+
+public class SavingAccount extends BankAccount {
+    final double interestRate;
+    protected double minimumBalance;
+
+    public SavingAccount(String accountNumber, String accountHolderName, double balance, double interestRate, double minimumBalance) {
+        super(accountNumber, accountHolderName, balance);
+        this.interestRate = interestRate;
+        this.minimumBalance = minimumBalance;
+    }
+
+    public SavingAccount(String accountNumber, String accountHolderName, double interestRate) {
+        super(accountNumber, accountHolderName);
+        this.interestRate = interestRate;
+
+    }
+
+    public void applyInterest (double years){
+        super.balance+= (1 + years * interestRate) * super.balance;
+
+    }
+     @Override
+     public void withdraw (double amount) throws InsufficientFundsException, InvalidTransactionException {
+         if (amount > balance)
+             throw new InsufficientFundsException("insufficient balance ");
+         else if (amount < 0)
+             throw new InsufficientFundsException("not valid amount ");
+         else if (balance- amount < minimumBalance)
+             throw new InvalidTransactionException("your balance should maintain bigger than minimumBalance");
+         else
+             balance -= amount;
+     }
 }
